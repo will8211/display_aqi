8b0871b34bbc7c311005ffca84e79bf8881a8bf3
will8211 <wmannard@lycos.com>
2018-02-03 20:33:21 +0800

    Moved js to separate files

--- www/js/aqifeed.js
+++ www/js/aqifeed.js
@@ -4,7 +4,7 @@ _aqiFeed({
   display:"<div style='%style;text-align:center;height:100vh;'>" +
             "<div style='font-size:8vh;height:5vh;'>%cityname Air Quality Index:</div>" +
             "<div style='font-size:65vh;height:65vh;'>%aqiv</div>" +
-            "<div style='font-size:15vh;height:20vh;font-weight:bold'>%impact</div>" +
+            "<div style='font-size:12vh;height:20vh;font-weight:bold'>%impact</div>" +
             "<div style='font-size:5vh;height:5vh;'>Updated on %date</div>" +
           "</div>",
   container:"city-aqi-container",
